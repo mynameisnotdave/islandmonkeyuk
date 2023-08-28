a6dfdb8fa67ae2274a1d7ca3a3e9d45b57dad01a
mynameisnotdave <islandmonkeee@gmail.com>
2023-08-28 16:05:55 +0100

    +add css file for TermsOfService

--- Controllers/UploadFile.cs
+++ Controllers/UploadFile.cs
@@ -1,6 +1,12 @@
-﻿namespace islandmonkeyuk.Controllers
+﻿using CsvHelper;
+
+namespace islandmonkeyuk.Controllers
 {
     public class UploadFile
     {
+        public void ProcessFile(FileStream file) {
+            CsvReader csvReader = new(new StringReader(File.Open("/home/aidan/IdeaProjects/MyFitnessPalData/Nutrition-Summary-2023-03-02-to-2023-04-04.csv", FileMode.Open)));
+        }
+        
     }
 }
