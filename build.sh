2447061a8b12e1a02541a70aba99b00c348e3143
mynameisnotdave <islandmonkeee@gmail.com>
2023-08-28 18:19:30 +0100

    see if this does the trick

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output -verbosity:diag
+./dotnet/dotnet publish -c Release -o output #-verbosity:diag
