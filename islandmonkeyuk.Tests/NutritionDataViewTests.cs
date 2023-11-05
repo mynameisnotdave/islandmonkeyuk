cdef8385ee05b3fff041a51a272abb0e71807071
mynameisnotdave <islandmonkeee@gmail.com>
2023-11-05 20:31:46 +0000

    bring back unit tests

---
+++ islandmonkeyuk.Tests/NutritionDataViewTests.cs
@@ -0,0 +1,17 @@
+namespace islandmonkeyuk.Tests;
+
+using Pages;
+
+public class Tests {
+    [SetUp]
+    public void Setup()
+    {
+        NutritionDataView nutritionDataView;
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
