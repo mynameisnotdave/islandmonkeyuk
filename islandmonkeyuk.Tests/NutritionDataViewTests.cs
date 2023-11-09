a6fb20c4c98d624b94791602a5ba4768b8fd64d0
mynameisnotdave <islandmonkeee@gmail.com>
2023-11-09 19:28:29 +0000

    tests tests tests

--- islandmonkeyuk.Tests/NutritionDataViewTests.cs
+++ islandmonkeyuk.Tests/NutritionDataViewTests.cs
@@ -1,17 +1,48 @@
 namespace islandmonkeyuk.Tests;
 
+using FluentAssertions;
 using Pages;
 
 public class Tests {
+    
+    NutritionDataView nutritionDataView;
+    
     [SetUp]
     public void Setup()
     {
-        NutritionDataView nutritionDataView;
+        nutritionDataView = new();
     }
+    
 
     [Test]
-    public void Test1()
+    public void GetCalorieValues_WhenCalled_DoesNotReturnNull()
     {
-        Assert.Pass();
+        nutritionDataView.GetCalorieValues().Should().NotBeNull();
     }
+    
+    [Test]
+    public void GetFatValues_WhenCalled_DoesNotReturnNull()
+    {
+        nutritionDataView.GetFatValues().Should().NotBeNull();
+    }
+    
+    [Test]
+    public void GetSatFatValues_WhenCalled_DoesNotReturnNull()
+    {
+        nutritionDataView.GetSatFatValues().Should().NotBeNull();
+    }
+    
+    [Test]
+    public void GetMonoUnsatFatValues_WhenCalled_DoesNotReturnNull()
+    {
+        nutritionDataView.GetMonoUnsatFatValues().Should().NotBeNull();
+    }
+    
+    [Test]
+    public void GetPolyFatValues_WhenCalled_DoesNotReturnNull()
+    {
+        nutritionDataView.GetPolyFatValues().Should().NotBeNull();
+    }
+    
+    
 }
